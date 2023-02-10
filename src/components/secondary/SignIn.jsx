f7df6213c0d80817496ea4ea88309b64e51a2c95
AtanasovCode <a_ljupce@yahoo.com>
2023-02-10 04:27:49 +0100

    added styled-components to auth

--- src/components/secondary/SignIn.jsx
+++ src/components/secondary/SignIn.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../../styles/auth-page.css';
+import * as Styled from '../../styles/Auth.Styled';
 import {
     signInWithEmailAndPassword,
     GoogleAuthProvider,
@@ -9,7 +10,7 @@ import {
 } from 'firebase/auth';
 
 const SignIn = ({ auth }) => {
-    
+
     const [mail, setMail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -41,51 +42,47 @@ const SignIn = ({ auth }) => {
     }
 
     return (
-        <div className="auth-container">
-            <div className="auth-nav-container">
-                <Link
-                    className="auth-logo"
-                    to="/"
-                >
-                    Fundlify
-                </Link>
-            </div>
-            <div className="auth-form-container sign-in-form">
-                <div className="auth-heading">Sign In</div>
-                <input
+        <Styled.Container>
+            <Styled.Logo to="/" >
+                Fundlify
+            </Styled.Logo>
+            <Styled.SignIn>
+                <Styled.Heading>Sign In</Styled.Heading>
+                <Styled.Input
                     type="email"
                     placeholder="Email"
-                    className="auth-input"
                     name="mail"
                     value={mail}
                     onChange={(e) => setMail(e.currentTarget.value)}
                 />
-                <input
+                <Styled.Input
                     type="password"
                     placeholder="Password"
-                    className="auth-input"
                     name="password"
                     value={password}
                     onChange={(e) => setPassword(e.currentTarget.value)}
                 />
-                <input
+                <Styled.Input
                     type="button"
                     value="Sign in"
-                    className="auth-btn"
                     onClick={handleSignIn}
                 />
-                <div className="other-options-container">
+                <Styled.AlternateOption>
                     or
-                    <div className="sign-up-google" onClick={handleSignInWithGoogle}>
-                        Continue with Google
-                    </div>
-                </div>
-                <div className="auth-change">
-                    New to Fundlify?
-                    <Link to="/sign-up" className="link">Sign Up</Link>
-                </div>
-            </div>
-        </div>
+                    <Styled.InputGoogle 
+                        onClick={handleSignInWithGoogle}
+                        type="button"
+                        value="Sign in with Google"
+                    />
+                </Styled.AlternateOption>
+                <Styled.NewUser>
+                    New to Fundlify? 
+                    <Styled.A to="/sign-up">
+                        Sign Up
+                    </Styled.A>
+                </Styled.NewUser>
+            </Styled.SignIn>
+        </Styled.Container>
     );
 }
 
---
+++ src/styles/Auth.Styled.js
@@ -0,0 +1,99 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Container = styled.div`
+    padding-top: 50px;
+    max-width: 1440px;
+    width: 100vw;
+    background-color: var(--main-bg);
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Logo = styled(Link)`
+    position: absolute;
+    top: 0;
+    font-size: 32px;
+    font-weight: 100;
+    text-decoration: none;
+    color: #ffffff;
+    max-width: 1440px;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 20px 0;
+`;
+
+export const SignIn = styled.div`
+    border: 1px solid #fff;
+    width: 80vw;
+    max-width: 450px;
+    padding: 5px;
+    margin: 50px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    border: 1px solid rgba(255, 255, 255, .4);
+    position: relative;
+`;
+
+
+export const Heading = styled.div`
+    font-size: 26px;
+    font-weight: 700;
+    margin: 40px 0;
+`;
+
+export const Input = styled.input`
+    height: 50px;
+    width: 80%;
+    margin-bottom: 30px;
+    background-color: transparent;
+    color: #fff;
+    padding-left: 10px;
+    border: 1px solid #ffffff40;
+    ${props => props.type === "button" && `cursor: pointer`};
+    &:hover {
+        ${props => props.type === "button" && `
+            background-color: #ffffff40
+        `};
+    }
+`;
+
+export const AlternateOption = styled.div`
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+`;
+
+export const InputGoogle = styled(Input)`
+    height: 60px;
+    background-color: #F7F4F3;
+    color: #000;
+    margin-top: 35px;
+    &:hover {
+        background-color: #CCC;
+    }
+`;
+
+export const NewUser = styled.div`
+    width: 80%;
+    text-align: center;
+    font-size: 15px;
+    font-weight: 300;
+`;
+
+export const A = styled(Link)`
+    text-decoration: none;
+    color: dodgerblue;
+    cursor: pointer;
+    display: inline-block;
+    margin-left: 8px;
+`;
